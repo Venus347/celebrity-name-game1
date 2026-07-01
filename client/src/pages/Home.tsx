8780c4056310dd7f1fe10aeea9c09bab190e3e18
Justice Kirton <kirtonjustice4@gmail.com>
2026-07-01 14:17:01 -0400

    Added Start Button and also Username input

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonInput, IonItem, IonList, Controller } from '@ionic/react';
+import { useForm} from 'react-hook-form';
 
-const Home: React.FC = () => {
+const Home = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      name: ''
+    }
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Celebrity Name Chain</IonTitle>
         </IonToolbar>
+         <IonItem>
+        <IonInput label="Username:" placeholder="Enter your username"></IonInput>
+      </IonItem>
       </IonHeader>
       <IonContent fullscreen>
-        <p>Hello, world!</p>
+          <IonButton expand="block" type="submit">Start Game</IonButton>
       </IonContent>
     </IonPage>
   );
